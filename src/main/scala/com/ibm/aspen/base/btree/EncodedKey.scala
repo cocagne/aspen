d7ba7c77ff44259e50a5bdb630c2b423f0d5cc24
Tom Cocagne <cocagne@us.ibm.com>
2017-09-27 08:21:06 -0500

    Removed BTree package and tests

--- src/main/scala/com/ibm/aspen/base/btree/EncodedKey.scala
+++
@@ -1,8 +0,0 @@
-package com.ibm.aspen.base.btree
-
-import java.nio.ByteBuffer
-
-trait EncodedKey extends Ordered[EncodedKey] {
-  val encoded: ByteBuffer
-  def compare(that: EncodedKey) = encoded.compareTo(that.encoded)
-}
