aae7c2c0462e210b5364e282b0ae20999e36201e
Tom Cocagne <cocagne@us.ibm.com>
2018-02-01 08:27:21 -0600

    Added hashCode to compliment overridden equals method in StorePointer

--- src/main/scala/com/ibm/aspen/core/objects/StorePointer.scala
+++ src/main/scala/com/ibm/aspen/core/objects/StorePointer.scala
@@ -21,4 +21,6 @@ final case class StorePointer(poolIndex: Byte, data: Array[Byte]) {
     case rhs: StorePointer => poolIndex == rhs.poolIndex && java.util.Arrays.equals(data, rhs.data)
     case _ => false
   }
+  
+  override def hashCode(): Int = java.util.Arrays.hashCode(data) ^ poolIndex
 }
